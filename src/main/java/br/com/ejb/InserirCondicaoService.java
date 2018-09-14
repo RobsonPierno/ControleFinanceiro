ebd9e00b84c075d0aefe62f5cf72e5997af16cd2
Robson Pierno de Jesus <ropierno@engdb.infra>
2018-09-14 09:57:55 -0300

    optimizing the if addition process

--- src/main/java/br/com/ejb/InserirCondicaoService.java
+++ src/main/java/br/com/ejb/InserirCondicaoService.java
@@ -1,5 +1,6 @@
 package br.com.ejb;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -9,24 +10,41 @@ import javax.ejb.Stateless;
 
 @Stateless
 public class InserirCondicaoService {
+	
+	private List<String> hierarquiaDeTags;
 
 	public String gerarOutput(String input) {
 		List<String> rows = Arrays.asList(input.split("\n"));
 		Map<String, String> valuesPaths = new HashMap<>();
+		this.setHierarquiaDeTags(new ArrayList<>());
+		
+		boolean afterHead = false;
 
 		String tag = "";
 		for(String row : rows) {
-		
-			if(row.contains("value-of")) {
-				int begin = row.indexOf("\"")+1;
-				int end = row.lastIndexOf("\"");
-				valuesPaths.put(tag, row.substring(begin, end));
+			
+			afterHead = !afterHead ? row.contains("<xsl:template") : true;
+			
+			if(!row.trim().equals("")) {
 				
-			}else if(!row.contains("for-each") && !row.trim().equals("")) {
-				int begin = row.contains(":") ? row.indexOf(":")+1 : 1;
-				int end = row.indexOf(">");
-				tag = row.substring(begin, end);
-				
+				if(row.contains("value-of")) {
+					int begin = row.indexOf("\"")+1;
+					int end = row.lastIndexOf("\"");
+					
+					String tagComPath = this.contruirEstruturaDaTag(); 
+					valuesPaths.put(tagComPath, row.substring(begin, end));
+					
+				}else if(this.isTagRow(afterHead, row)) {
+					int begin = row.contains(":") ? row.indexOf(":")+1 : row.indexOf("<")+1;
+					int end = row.indexOf(">");
+					tag = row.substring(begin, end);
+					
+					if(row.contains("</")) {
+						this.getHierarquiaDeTags().remove(tag);
+					}else {
+						this.getHierarquiaDeTags().add(tag);
+					}
+				}
 			}
 		}
 		
@@ -36,20 +54,30 @@ public class InserirCondicaoService {
 	private String contruirOutputComCondicoes(List<String> rows, Map<String, String> valuesPaths) {
 		StringBuilder output = new StringBuilder();
 		
-		rows.forEach(row -> {
-			if(!row.trim().equals("") && !row.contains("head")) {
-				int begin = row.contains(":") ? row.indexOf(":")+1 : 1;
+		this.setHierarquiaDeTags(new ArrayList<>());
+		boolean afterHead = false;
+		
+		for(String row : rows) {
+			afterHead = !afterHead ? row.contains("<xsl:template") : true;
+			
+			if(this.isTagRow(afterHead, row)) {
+				int begin = row.contains(":") ? row.indexOf(":")+1 : row.indexOf("<")+1;
 				int end = row.indexOf(">");
 				
 				String tag = row.substring(begin, end);
-				if(valuesPaths.containsKey(tag)) {
+				if(!row.contains("</")) {
+					this.getHierarquiaDeTags().add(tag);
+				}
+				
+				String tagComPath = this.contruirEstruturaDaTag(); 
+				if(valuesPaths.containsKey(tagComPath)) {
 					if(!row.contains("/") ) {
 						String space = row.substring(0, row.indexOf("<"));
 						
 						output.append("\n");
 						output.append(space);
 						output.append("<xsl:if test=\"");
-						output.append(valuesPaths.get(tag));
+						output.append(valuesPaths.get(tagComPath));
 						output.append("\">");
 						
 						output.append("\n");
@@ -73,12 +101,51 @@ public class InserirCondicaoService {
 					output.append("\n");
 					output.append(row);
 				}
+				
+				if(row.contains("</")) {
+					this.getHierarquiaDeTags().remove(tag);
+				}
 			}else {
-				output.append("\n");
-				output.append(row);
+				if(!row.contains("if test") && !row.contains("if>")) {
+					output.append("\n");
+					if(row.contains("value-of")) {
+						output.append("	");
+					}
+					output.append(row);
+				}
 			}
-		});
+		}
 		
 		return output.toString();
+	}
+
+	private String contruirEstruturaDaTag() {
+		StringBuilder tagComPath = new StringBuilder();
+		
+		this.getHierarquiaDeTags().forEach(tagFromList -> {
+			tagComPath.append(tagFromList);
+			tagComPath.append("/");
+		});
+		
+		return tagComPath.toString();
+	}
+	
+	private boolean isTagRow(boolean afterHead, String row) {
+		return 	afterHead &&
+				!row.trim().equals("") && 
+				row.contains("<") && 
+				row.contains(">") && 
+				!row.contains("value-of") && 
+				!row.contains("for-each") &&
+				!row.contains("if test") &&
+				!row.contains("if>");
+	}
+
+	public List<String> getHierarquiaDeTags() {
+		return hierarquiaDeTags;
+	}
+
+	public void setHierarquiaDeTags(List<String> hierarquiaDeTags) {
+		this.hierarquiaDeTags = hierarquiaDeTags;
 	}
 }
